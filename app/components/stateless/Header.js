4e340764f92a67181ec529e863d9eb2d39219dbe
Mandarin Drummond <mandarind@gmail.com>
2016-03-18 14:50:09 -0400

    typo

--- app/components/stateless/Header.js
+++ app/components/stateless/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
         strong believer in transparency and the "fail-fast" culture/mentality.
         I try to spend my time as efficiently as possible, and I'm always on the lookout for ways to improve productivity.
         I enjoy trying new things and sharing what I know with others.
-        I consider my my development "instincts" and efficiency to be my greatest assets.
+        I consider my development "instincts" and efficiency to be my greatest assets.
       </p>
       <p>
         The last few years I've been riding the JavaScript wave. I find the JavaScript ecosystem exciting and dynamic, but I'm also interested
@@ -34,7 +34,7 @@ const Header = () => {
 
       <p>
         This resume is published via <a href="https://pages.github.com/">GitHub pages</a>.
-        It was written with <a href="https://facebook.github.io/react/">React</a>+<a href="http://redux.js.org/">Redux</a>, among <a href="https://github.com/MandarinConLaBarba/resume/blob/master/package.json">other things</a>. 
+        It was written with <a href="https://facebook.github.io/react/">React</a>+<a href="http://redux.js.org/">Redux</a>, among <a href="https://github.com/MandarinConLaBarba/resume/blob/master/package.json">other things</a>.
         The source is open and available <a href="https://github.com/MandarinConLaBarba/resume">here</a>.
       </p>
 
